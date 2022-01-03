a9f1969dcdf2ead9191a0cc8933372e090bc8f7a
Naresh Personal Git account <vadalanaresh@gmail.com>
2022-01-04 00:39:35 +0530

    Exercise files

--- src/components/expense/Expenses.jsx
+++ src/components/expense/Expenses.jsx
@@ -1,10 +1,21 @@
+import { useState } from "react";
 import ExpenseItem from "./ExpenseItem";
 import Card from "../ui/Card";
+import ExpensesFilter from "./ExpensesFilter";
+
 import "./Expense.css";
 
 const Expenses = (props) => {
+  const [selectedYear, setSelectedYear] = useState("");
+  const selectFilterChangeHandler = (selectedYear) => {
+    setSelectedYear(selectedYear);
+  };
+
   return (
     <Card className="expenses">
+      <ExpensesFilter
+        onSelectFilterChange={selectFilterChangeHandler}
+      ></ExpensesFilter>
       {props.expenses.map((expense) => (
         <ExpenseItem
           title={expense.title}
